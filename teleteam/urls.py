eb4ebcd6969096cf387e74bb7932b6fbdf798908
Ivander <ivanderjmw@gmail.com>
2020-07-26 15:49:19 +0800

    Connect to React app

--- teleteam/urls.py
+++ teleteam/urls.py
@@ -14,10 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf.urls import url
 from django.conf.urls.static import static
 from django.conf import settings
+from django.views.generic import TemplateView
 
 from teleteam_bot.recurring_tasks import setup_recurring_tasks
 
@@ -26,6 +27,7 @@ urlpatterns = [
     url(r'^', include('django_telegrambot.urls')),
     path('api-auth/', include('rest_framework.urls')),
     path('api/', include('main_app.api.urls'))
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) 
++ [re_path('.*', TemplateView.as_view(template_name='index.html'))]
 
 # setup_recurring_tasks()
