41072627ec566dae33c9af143d6cd163420e5d35
Ivander <ivanderjmw@gmail.com>
2020-07-27 10:58:30 +0800

    Allow all cors

--- teleteam/settings.py
+++ teleteam/settings.py
@@ -254,10 +254,7 @@ REST_FRAMEWORK = {
     ],
 }
 
-# Django CORS HEADERS
-CORS_ORIGIN_WHITELIST = (
-    'https://localhost:3000',
-)
+CORS_ORIGIN_ALLOW_ALL = True
 
 CORS_ALLOW_HEADERS = list(default_headers) + [
     'token',
